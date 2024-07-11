d4f7c1c1036644c4de3ac764e0f239e3a5c0a614
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-07-11 11:01:28 +0900

    armgcc: remove unsupported targets
    remove *.bat, flash builds

--- armgcc/build_flash_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $(dirname $(readlink -f $0))
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="${SdkRootDirPath}/core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_debug  .
-make -j 2>&1 | tee build_log.txt
