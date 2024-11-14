969d3270dcba8b6d602d72af4c7f0e12ea665db2
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-11-14 17:11:16 +0900

    retarget dual 5 variant: s/MIMX8UD7/MIMX8UD5/
    This does not fix anything, but does not seem to hurt either, and should
    avoid further potential problems
    
    Change-Id: I6f0115051f479ece81d58367aacd18546a901e59

--- armgcc/flags.cmake
+++ armgcc/flags.cmake
@@ -33,7 +33,7 @@ SET(CMAKE_ASM_FLAGS_RELEASE " \
 SET(CMAKE_C_FLAGS_DEBUG " \
     ${CMAKE_C_FLAGS_DEBUG} \
     -DDEBUG \
-    -DCPU_MIMX8UD7DVP08_cm33 \
+    -DCPU_MIMX8UD5DVP08_cm33 \
     -DSDK_I2C_BASED_COMPONENT_USED=1 \
     -DBOARD_USE_PCA6416A=1 \
     -DBOARD_USE_TPM=1 \
@@ -63,7 +63,7 @@ SET(CMAKE_C_FLAGS_DEBUG " \
 SET(CMAKE_C_FLAGS_RELEASE " \
     ${CMAKE_C_FLAGS_RELEASE} \
     -DNDEBUG \
-    -DCPU_MIMX8UD7DVP08_cm33 \
+    -DCPU_MIMX8UD5DVP08_cm33 \
     -DSDK_I2C_BASED_COMPONENT_USED=1 \
     -DBOARD_USE_PCA6416A=1 \
     -DBOARD_USE_TPM=1 \
@@ -163,7 +163,7 @@ SET(CMAKE_EXE_LINKER_FLAGS_DEBUG " \
     --defsym=__heap_size__=0x400 \
     ${FPU} \
     ${SPECS} \
-    -T\"${ProjDirPath}/MIMX8UD7xxxxx_cm33_ram.ld\" -static \
+    -T\"${ProjDirPath}/MIMX8UD5xxxxx_cm33_ram.ld\" -static \
 ")
 SET(CMAKE_EXE_LINKER_FLAGS_RELEASE " \
     ${CMAKE_EXE_LINKER_FLAGS_RELEASE} \
@@ -193,5 +193,5 @@ SET(CMAKE_EXE_LINKER_FLAGS_RELEASE " \
     --defsym=__heap_size__=0x400 \
     ${FPU} \
     ${SPECS} \
-    -T\"${ProjDirPath}/MIMX8UD7xxxxx_cm33_ram.ld\" -static \
+    -T\"${ProjDirPath}/MIMX8UD5xxxxx_cm33_ram.ld\" -static \
 ")
--- boards/a900/set_board_evkmimx8ulp.cmake
+++ boards/a900/set_board_evkmimx8ulp.cmake
@@ -6,7 +6,7 @@ if (CONFIG_USE_BOARD_Project_Template_evkmimx8ulp)
 
 message("BOARD_Project_Template_evkmimx8ulp component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_BOARD STREQUAL evkmimx8ulp) AND CONFIG_USE_component_lpuart_adapter AND (CONFIG_DEVICE_ID STREQUAL MIMX8UD7xxx08) AND CONFIG_USE_device_MIMX8UD7_startup AND CONFIG_USE_driver_cache_cache64 AND CONFIG_USE_driver_clock AND CONFIG_USE_driver_common AND CONFIG_USE_driver_flexspi AND CONFIG_USE_driver_fusion AND CONFIG_USE_driver_iomuxc AND CONFIG_USE_driver_lpuart AND CONFIG_USE_driver_reset AND CONFIG_USE_driver_rgpio AND CONFIG_USE_driver_upower AND ((CONFIG_USE_component_serial_manager AND CONFIG_USE_utility_assert AND CONFIG_USE_utility_debug_console) OR (CONFIG_USE_utility_assert_lite AND CONFIG_USE_utility_debug_console_lite)))
+if((CONFIG_BOARD STREQUAL evkmimx8ulp) AND CONFIG_USE_component_lpuart_adapter AND (CONFIG_DEVICE_ID STREQUAL MIMX8UD5xxx08) AND CONFIG_USE_device_MIMX8UD5_startup AND CONFIG_USE_driver_cache_cache64 AND CONFIG_USE_driver_clock AND CONFIG_USE_driver_common AND CONFIG_USE_driver_flexspi AND CONFIG_USE_driver_fusion AND CONFIG_USE_driver_iomuxc AND CONFIG_USE_driver_lpuart AND CONFIG_USE_driver_reset AND CONFIG_USE_driver_rgpio AND CONFIG_USE_driver_upower AND ((CONFIG_USE_component_serial_manager AND CONFIG_USE_utility_assert AND CONFIG_USE_utility_debug_console) OR (CONFIG_USE_utility_assert_lite AND CONFIG_USE_utility_debug_console_lite)))
 
 add_config_file(${CMAKE_CURRENT_LIST_DIR}/project_template/board.h "" BOARD_Project_Template_evkmimx8ulp)
 add_config_file(${CMAKE_CURRENT_LIST_DIR}/project_template/board.c "" BOARD_Project_Template_evkmimx8ulp)
