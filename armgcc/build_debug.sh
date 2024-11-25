ceca536fb7cdb8d391184a896d27d13c783bb118
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-11-25 14:49:05 +0900

    build_debug: fix debug build
    - make build_debug work like build_release
    - make debug build build with -DNDEBUG, as -DDEBUG does not boot
    - while here cleanup flags.cmake a bit (remove duplicate flag, cleanup
      trailing whitespace)
    
    Change-Id: I15f5b24fc0750773db2ec99cf71dde4c8f72c4d6

--- armgcc/build_debug.sh
+++ armgcc/build_debug.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
-cd $(dirname $(readlink -f $0))
+#!/bin/bash
+
+# for `make | tee`
+set -o pipefail
+set -xe
+
+workdir="$(dirname $(readlink -f $0))"
+
+cd "$workdir" || exit 1
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="${SdkRootDirPath}/core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
+cmake -DCMAKE_TOOLCHAIN_FILE="${SdkRootDirPath}/core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug
+
+[ -e debug/sdk20-app.bin ] && mv debug/sdk20-app.bin debug/sdk20-app.bin.old
 make -j 2>&1 | tee build_log.txt
+if [ -e debug/sdk20-app.bin ]; then cp debug/sdk20-app.bin ../m33-firmware-at.bin; rm -f debug/sdk20-app.bin.old;
+else mv debug/sdk20-app.bin.old debug/sdk20-app.bin; fi
