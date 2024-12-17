atmark-techno/m33-firmware-at

commit ee32544fbff0ba95c17edbe21d7c9f5c04ad6e05

srtm_adc: clarify we copy request_id to reply
we currently memcpy the whole request to response but we only care
about the request id here

Change-Id: I3f60e76040c629aed4c23fa9fdd784a658a9a038

srtm/services/srtm_adc_service.c
@@ -91,7 +91,7 @@ static srtm_status_t SRTM_AdcService_Request(srtm_service_t service, srtm_reques
         SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_INFO,
                            "SRTM receive ADC request: cmd = 0x%x, idx = 0x%x\r\n",
                            command, adcReq->idx);
-        memcpy(adcResp, adcReq, sizeof(struct _srtm_adc_payload));
+        adcResp->requestID = adcReq->requestID;
         switch (command)
         {
             case SRTM_ADC_CMD_GET:

commit c18b8a2f9d6e923245b32a3e70c409aa8d851b0e

ADC service: fix usage of uninitialized adcResp->idx
Cleanup commit removed a memcpy req -> resp that looked (and should have
been) useless, but get() was called using both adcReq->idx and
adcResp->idx (?!?!) and that typo now started causing ADC conversions to
not trigger, and hang.

- always use adcReq
- memset response to avoid exposing uninitalized memory to linux

Fixes: ee32544fbff0 ("srtm_adc: clarify we copy request_id to reply")
Change-Id: I2e6e8010ac345457675baa0a44d7bad2ab5bce11

srtm/services/srtm_adc_service.c
@@ -72,6 +72,8 @@ static srtm_status_t SRTM_AdcService_Request(srtm_service_t service, srtm_reques
     }
 
     adcResp = (struct _srtm_adc_payload *)(void *)SRTM_CommMessage_GetPayload(response);
+    /* don't leak uninitialized values to linux */
+    memset(adcResp, 0, sizeof(*adcResp));
 
     status = SRTM_Service_CheckVersion(service, request, SRTM_ADC_VERSION);
     if ((status != SRTM_Status_Success) || (adcReq == NULL) || (payloadLen != sizeof(struct _srtm_adc_payload)))
@@ -96,11 +98,17 @@ static srtm_status_t SRTM_AdcService_Request(srtm_service_t service, srtm_reques
             case SRTM_ADC_CMD_GET:
                 assert(adapter->get);
 
-                status = adapter->get(&adapter->handles[adcReq->idx], adcResp->idx, &value);
+                status = adapter->get(&adapter->handles[adcReq->idx], adcReq->idx, &value);
                 SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_DEBUG, "Got ADC value %d\r\n", value);
-                adcResp->value = value;
-                adcResp->retCode =
-                    status == SRTM_Status_Success ? SRTM_ADC_RETURN_CODE_SUCCESS : SRTM_ADC_RETURN_CODE_FAIL;
+                if (status == SRTM_Status_Success)
+                {
+                    adcResp->value   = value;
+                    adcResp->retCode = SRTM_ADC_RETURN_CODE_SUCCESS;
+                }
+                else
+                {
+                    adcResp->retCode = SRTM_ADC_RETURN_CODE_FAIL;
+                }
                 break;
             default:
                 SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_WARN, "%s: command %d unsupported\r\n", __func__, command);