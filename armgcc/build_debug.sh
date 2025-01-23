6fc5219a10dd68a2c52b393b6d130cb8c5577246
Dominique Martinet <dominique.martinet@atmark-techno.com>
2025-01-23 10:15:45 +0900

    armgcc/build_{release,debug}.sh: do not clean all the time
    CMake is actually good with dependencies, keep old build files
    if the build was configured with identical options.
    
    This makes iterative builds much faster
    
    Change-Id: I0cd39d011fb2a74c32d08562273b9fb9c263eeb8

--- armgcc/build_debug.sh
+++ armgcc/build_debug.sh
@@ -4,16 +4,24 @@
 set -o pipefail
 set -xe
 
-workdir="$(dirname $(readlink -f $0))"
+if [ -z "$SdkRootDirPath" ]; then
+	echo "\$SdkRootDirPath must be set!" >&2
+	exit 1
+fi
+
+workdir="$(dirname "$(readlink -f "$0")")"
 
 cd "$workdir" || exit 1
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="${SdkRootDirPath}/core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug
 
-[ -e debug/sdk20-app.bin ] && mv debug/sdk20-app.bin debug/sdk20-app.bin.old
+# clear cache unless already configure in same mode with same toolchain
+if ! [ -e Makefile ] \
+    || ! grep -q CMAKE_BUILD_TYPE:STRING=debug CMakeCache.txt \
+    || ! grep -q "CMAKE_TOOLCHAIN_FILE:FILEPATH=$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" CMakeCache.txt; then
+	rm -rf CMakeFiles Makefile cmake_install.cmake CMakeCache.txt debug/sdk20-app.bin
+	cmake -DCMAKE_TOOLCHAIN_FILE="$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" \
+		-G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug
+fi
+
 make -j 2>&1 | tee build_log.txt
-if [ -e debug/sdk20-app.bin ]; then cp debug/sdk20-app.bin ../m33-firmware-at.bin; rm -f debug/sdk20-app.bin.old;
-else mv debug/sdk20-app.bin.old debug/sdk20-app.bin; fi
+
+cp debug/sdk20-app.bin ../m33-firmware-at.bin
--- armgcc/build_release.sh
+++ armgcc/build_release.sh
@@ -4,16 +4,24 @@
 set -o pipefail
 set -xe
 
-workdir="$(dirname $(readlink -f $0))"
+if [ -z "$SdkRootDirPath" ]; then
+	echo "\$SdkRootDirPath must be set!" >&2
+	exit 1
+fi
+
+workdir="$(dirname "$(readlink -f "$0")")"
 
 cd "$workdir" || exit 1
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="${SdkRootDirPath}/core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release
 
-[ -e release/sdk20-app.bin ] && mv release/sdk20-app.bin release/sdk20-app.bin.old
+# clear cache unless already configure in same mode with same toolchain
+if ! [ -e Makefile ] \
+    || ! grep -q CMAKE_BUILD_TYPE:STRING=release CMakeCache.txt \
+    || ! grep -q "CMAKE_TOOLCHAIN_FILE:FILEPATH=$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" CMakeCache.txt; then
+	rm -rf CMakeFiles Makefile cmake_install.cmake CMakeCache.txt release/sdk20-app.bin
+	cmake -DCMAKE_TOOLCHAIN_FILE="$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" \
+		-G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release
+fi
+
 make -j 2>&1 | tee build_log.txt
-if [ -e release/sdk20-app.bin ]; then cp release/sdk20-app.bin ../m33-firmware-at.bin; rm -f release/sdk20-app.bin.old;
-else mv release/sdk20-app.bin.old release/sdk20-app.bin; fi
+
+cp release/sdk20-app.bin ../m33-firmware-at.bin
