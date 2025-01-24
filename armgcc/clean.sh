2e66fb6fe7af4ba275f2acf74bd0546a12d222db
Dominique Martinet <dominique.martinet@atmark-techno.com>
2025-01-24 13:10:45 +0900

    misc: add missing license headers
    Some files were lacking license header
    
    Also remove build_all.sh that was never used
    
    Change-Id: I8ce02451ad88089d58c3bdb0c9eda8533e08e921

--- armgcc/clean.sh
+++ armgcc/clean.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-workdir="$(dirname $(readlink -f $0))"
-rm -rf "$workdir"/debug "$workdir"/release "$workdir"/flash_debug "$workdir"/flash_release "$workdir"/CMakeFiles
+# SPDX-License-Identifier: BSD-3-Clause
+
+workdir="$(dirname "$(readlink -f "$0")")"
+
+rm -rf "$workdir"/debug "$workdir"/release "$workdir"/CMakeFiles
 rm -rf "$workdir"/Makefile "$workdir"/cmake_install.cmake "$workdir"/CMakeCache.txt
 rm -rf "$workdir"/../m33-firmware-at.bin
--- lpuart.c
+++ lpuart.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2025 Atmark Techno
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include "fsl_lpuart.h"
 
 /*!
