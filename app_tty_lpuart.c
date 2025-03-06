atmark-techno/m33-firmware-at

commit 9e71427505074169081c7897d1b46879a04865bf

app_tty: add activate call
This avoids linux buffer growing full if it is not actively listening

Change-Id: I680782cbb7d68520a7ae369d2335dbc035741e6d

app_tty_lpuart.c
@@ -29,7 +29,7 @@ struct lpuart_tty_settings
     uint32_t rs485_de_gpio;
     uint32_t cflag;
     uint32_t suspend_wakeup_gpio; /* rx pin to use for wakeup */
-    bool in_suspend;
+    enum tty_state state;
     bool wakeup_source;
     TaskHandle_t rx_task;
     lpuart_rtos_handle_t lpuart_rtos_handle;
@@ -52,6 +52,10 @@ static void lpuart_tty_rx_task(void *pvParameters)
     assert(setting);
     assert(setting->type == TTY_TYPE_LPUART);
 
+    /* if task was created during suspend make it wait here... */
+    if (settings->state & TTY_SUSPENDED)
+        vTaskSuspend(NULL);
+
     uint8_t *buf;
     uint16_t maxlen;
     while (true)
@@ -69,7 +73,9 @@ static void lpuart_tty_rx_task(void *pvParameters)
         do
         {
             LPUART_RTOS_Receive(&lpuart->lpuart_rtos_handle, buf, maxlen, &recvlen);
-            if (lpuart->in_suspend)
+
+            /* suspend if not active or suspended */
+            if ((settings->state & (TTY_ACTIVE | TTY_SUSPENDED)) != TTY_ACTIVE)
                 vTaskSuspend(NULL);
         } while (recvlen == 0);
 
@@ -82,10 +88,9 @@ static int lpuart_tx(struct tty_settings *settings, uint8_t *buf, uint16_t len)
     struct lpuart_tty_settings *lpuart = get_lpuart(settings);
     int rc;
 
-    // not yet ready (still in suspend?)
-    if (!lpuart->lpuart_rtos_handle.base)
+    if (settings->state & TTY_SUSPENDED)
     {
-        PRINTF("tty %d write before device init\r\n", settings->port_idx);
+        PRINTF("tty %d write while not ready (state %x)\r\n", settings->port_idx, settings->state);
         return kStatus_Fail;
     }
 
@@ -105,6 +110,12 @@ static int lpuart_setcflag(struct tty_settings *settings, tcflag_t cflag)
     bool cmsparity                     = tty_cmsparity(cflag);
     lpuart_data_bits_t databits        = tty_databits(cflag);
     lpuart_stop_bit_count_t stopbits   = tty_stopbits(cflag);
+
+    if (settings->state & TTY_SUSPENDED)
+    {
+        PRINTF("tty %d setcflags while not ready (state %x)\r\n", settings->port_idx, settings->state);
+        return kStatus_Fail;
+    }
 
     /*
      * only support CS8 and CS7, and for CS7 must enable parity.
@@ -150,6 +161,30 @@ static int lpuart_setwake(struct tty_settings *settings, bool enable)
         return enable; /* failure if enabled */
 
     lpuart->wakeup_source = enable;
+    return 0;
+}
+
+static int lpuart_activate(struct tty_settings *settings)
+{
+    struct lpuart_tty_settings *lpuart = get_lpuart(settings);
+
+    if (settings->state & TTY_ACTIVE)
+    {
+        /* create task on first open */
+        if (!lpuart->rx_task)
+        {
+            /* we need this task to be higher priority than HandleSuspendTask in main,
+             * in order to exit out of LPUART_RTOS_Receive safely as suspend deinits it */
+            BUILD_BUG_ON(TTY_RX_TASK_PRIORITY <= SUSPEND_TASK_PRIORITY);
+            xTaskCreate(lpuart_tty_rx_task, "lpuart rx task", 256U, (void *)settings, TTY_RX_TASK_PRIORITY,
+                        &lpuart->rx_task);
+        }
+        else
+        {
+            vTaskResume(lpuart->rx_task);
+        }
+    }
+
     return 0;
 }
 
@@ -258,27 +293,17 @@ static int lpuart_init(struct tty_settings *settings, struct srtm_tty_init_paylo
     if (rc)
         return rc;
 
-    /* we need this task to be higher priority than HandleSuspendTask in main,
-     * in order to exit out of LPUART_RTOS_Receive safely as suspend deinits it */
-    BUILD_BUG_ON(TTY_RX_TASK_PRIORITY <= SUSPEND_TASK_PRIORITY);
-    xTaskCreate(lpuart_tty_rx_task, "lpuart rx task", 256U, (void *)settings, TTY_RX_TASK_PRIORITY, &lpuart->rx_task);
-
     return 0;
-}
-
-void lpuart_suspendTask(struct tty_settings *settings)
-{
-    struct lpuart_tty_settings *lpuart = get_lpuart(settings);
-
-    lpuart->in_suspend = true;
 }
 
 void lpuart_resumeTask(struct tty_settings *settings)
 {
     struct lpuart_tty_settings *lpuart = get_lpuart(settings);
 
-    lpuart->in_suspend = false;
-    vTaskResume(lpuart->rx_task);
+    if (lpuart->state & TTY_ACTIVE)
+    {
+        vTaskResume(lpuart->rx_task);
+    }
 }
 
 void lpuart_suspend(struct tty_settings *settings)
@@ -313,8 +338,8 @@ const struct tty_hooks tty_lpuart_hooks = {
     .tx            = lpuart_tx,
     .setcflag      = lpuart_setcflag,
     .setwake       = lpuart_setwake,
+    .activate      = lpuart_activate,
     .init          = lpuart_init,
-    .suspendTask   = lpuart_suspendTask,
     .resumeTask    = lpuart_resumeTask,
     .suspend       = lpuart_suspend,
     .resume        = lpuart_resume,

app_tty.c
@@ -92,6 +92,32 @@ static int APP_TTY_setwake(uint8_t port_idx, bool enable)
     return tty_hooks[settings->type]->setwake(settings, enable);
 }
 
+static int APP_TTY_activate(uint8_t port_idx, bool active)
+{
+    struct tty_settings *settings = get_settings(port_idx, "activate");
+
+    if (!settings)
+        return kStatus_Fail;
+
+    if (((active && (settings->state & TTY_ACTIVE)) || ((!active && !(settings->state & TTY_ACTIVE)))))
+    {
+        PRINTF("active state re-set ? (active %d, state %x)\r\n", active, settings->state);
+        return kStatus_Success;
+    }
+
+    if (active)
+        settings->state |= TTY_ACTIVE;
+    else
+        settings->state &= ~TTY_ACTIVE;
+
+    if (!tty_hooks[settings->type]->activate)
+        return kStatus_Success;
+
+    PRINTF("tty port %d activate %d\r\n", port_idx, active);
+
+    return tty_hooks[settings->type]->activate(settings);
+}
+
 static int APP_TTY_init(uint8_t port_idx, struct srtm_tty_init_payload *init)
 {
     struct tty_settings *settings;
@@ -145,7 +171,7 @@ static int APP_TTY_init(uint8_t port_idx, struct srtm_tty_init_payload *init)
 
 void APP_TTY_InitService(void)
 {
-    ttyService = SRTM_TtyService_Create(APP_TTY_tx, APP_TTY_setcflag, APP_TTY_setwake, APP_TTY_init);
+    ttyService = SRTM_TtyService_Create(APP_TTY_tx, APP_TTY_setcflag, APP_TTY_setwake, APP_TTY_init, APP_TTY_activate);
     SRTM_Dispatcher_RegisterService(disp, ttyService);
 }
 
@@ -159,6 +185,8 @@ void APP_TTY_SuspendTask(void)
 
         if (!settings)
             continue;
+
+        settings->state |= TTY_SUSPENDED;
 
         if (tty_hooks[settings->type]->suspendTask)
             tty_hooks[settings->type]->suspendTask(settings);
@@ -175,6 +203,8 @@ void APP_TTY_ResumeTask(void)
 
         if (!settings)
             continue;
+
+        settings->state &= ~TTY_SUSPENDED;
 
         if (tty_hooks[settings->type]->resumeTask)
             tty_hooks[settings->type]->resumeTask(settings);

app_tty.h
@@ -10,10 +10,28 @@
 
 #include "srtm_tty_service.h"
 
+/*
+ * state machine shared for all ttys:
+ * starts at 0
+ * activate toggles ACTIVE bit
+ *    (before calling activate hook)
+ * suspend toggles SUSPENDED bit
+ *    (before calling suspendTask/resumeTask hooks)
+ * in particular note resume() runs before resumeTask (and suspend() after
+ * suspendTask()), so if resume/suspend temporarily kills the hardware then
+ * it is safe to rely on the suspended bit for that.
+ */
+enum tty_state
+{
+    TTY_ACTIVE    = (1 << 0),
+    TTY_SUSPENDED = (1 << 1),
+} state;
+
 struct tty_settings
 {
     enum tty_rpmsg_init_type type;
     uint8_t port_idx;
+    enum tty_state state;
     /* per-type data follows, force alignment to ensure 'settings + 1' works */
 } __attribute__((aligned(4)));
 
@@ -23,6 +41,7 @@ struct tty_hooks
     int (*setcflag)(struct tty_settings *settings, tcflag_t cflag);
     int (*setwake)(struct tty_settings *settings, bool enable);
     int (*init)(struct tty_settings *settings, struct srtm_tty_init_payload *init);
+    int (*activate)(struct tty_settings *settings);
     void (*suspendTask)(struct tty_settings *settings);
     void (*resumeTask)(struct tty_settings *settings);
     void (*suspend)(struct tty_settings *settings);

app_tty_console.c
@@ -14,7 +14,7 @@
 static char rx_buf[RX_BUF_LEN];
 static uint16_t rx_start;
 static uint16_t rx_end;
-static uint8_t port_idx = 255;
+static struct tty_settings *console_settings;
 
 /* tx from linux = send to m33 console.
  * We can't interrupt it so keep in rx_buf until we're polled */
@@ -61,7 +61,13 @@ static int console_tx(struct tty_settings *settings, uint8_t *buf, uint16_t len)
 
 static int console_init(struct tty_settings *settings, struct srtm_tty_init_payload *generic_init)
 {
-    port_idx = settings->port_idx;
+    if (console_settings)
+    {
+        PRINTF("Trying to init m33 console multiple times! (%d -> %d)\r\n", console_settings->port_idx,
+               settings->port_idx);
+        return 1;
+    }
+    console_settings = settings;
 
     PRINTF("initialized tty %d for M33 console\r\n", settings->port_idx);
 
@@ -92,14 +98,18 @@ int APP_TTY_Console_Getchar(uint8_t *ch)
 void APP_TTY_Console_Write(uint8_t *buf, uint16_t len)
 {
     /* not init */
-    if (port_idx == 255)
+    if (!console_settings)
+        return;
+
+    /* skip if not active or suspended */
+    if ((console_settings->state & (TTY_ACTIVE | TTY_SUSPENDED)) != TTY_ACTIVE)
         return;
 
     while (len > 0)
     {
         uint8_t *back_buf;
         uint16_t maxlen;
-        srtm_notification_t notif = SRTM_TtyService_NotifyAlloc(port_idx, &back_buf, &maxlen);
+        srtm_notification_t notif = SRTM_TtyService_NotifyAlloc(console_settings->port_idx, &back_buf, &maxlen);
 
         /* truncate to whatever srtm sent us */
         uint16_t num = MIN(maxlen, len);

app_tty_flexio.c
@@ -33,8 +33,6 @@ struct flexio_tty_settings
     uint8_t tx_pin;
     uint32_t suspend_wakeup_gpio;
     uint32_t cflag;
-    bool in_suspend;
-    bool device_init;
     bool wakeup_source;
     TaskHandle_t rx_task;
     EventGroupHandle_t rx_event;
@@ -80,6 +78,10 @@ static void flexio_tty_rx_task(void *pvParameters)
     assert(setting);
     assert(setting->type == TTY_TYPE_FLEXIO);
 
+    /* if task was created during suspend make it wait here... */
+    if (settings->state & TTY_SUSPENDED)
+        vTaskSuspend(NULL);
+
     uint8_t *buf, *tmp_buf;
     uint16_t maxlen, next_maxlen, tmp_maxlen, tmp_maxlen2, recv_len;
     srtm_notification_t notif, next_notif, tmp_notif, tmp_notif2;
@@ -96,7 +98,8 @@ static void flexio_tty_rx_task(void *pvParameters)
 
     while (true)
     {
-        if (flexio->in_suspend)
+        /* suspend if not active or suspended */
+        if ((settings->state & (TTY_ACTIVE | TTY_SUSPENDED)) != TTY_ACTIVE)
             vTaskSuspend(NULL);
 
         /* 3 cases:
@@ -186,10 +189,9 @@ static int flexio_tx(struct tty_settings *settings, uint8_t *buf, uint16_t len)
                .dataSize = len,
     };
 
-    // not yet ready (still in suspend?)
-    if (!flexio->device_init)
+    if (settings->state & TTY_SUSPENDED)
     {
-        PRINTF("tty %d write before device init\r\n", settings->port_idx);
+        PRINTF("tty %d write while not ready (state %x)\r\n", settings->port_idx, settings->state);
         return kStatus_Fail;
     }
 
@@ -203,6 +205,12 @@ static int flexio_tx(struct tty_settings *settings, uint8_t *buf, uint16_t len)
 static int flexio_setcflag(struct tty_settings *settings, tcflag_t cflag)
 {
     struct flexio_tty_settings *flexio = get_flexio(settings);
+
+    if (settings->state & TTY_SUSPENDED)
+    {
+        PRINTF("tty %d setcflags while not ready (state %x)\r\n", settings->port_idx, settings->state);
+        return kStatus_Fail;
+    }
 
     PRINTF("TODO: flexio cflags ignored\r\n");
     /* XXX re-do device init?
@@ -222,6 +230,30 @@ static int flexio_setwake(struct tty_settings *settings, bool enable)
         return enable; /* failure if enabled */
 
     flexio->wakeup_source = enable;
+    return 0;
+}
+
+static int flexio_activate(struct tty_settings *settings)
+{
+    struct flexio_tty_settings *flexio = get_flexio(settings);
+
+    if (settings->state & TTY_ACTIVE)
+    {
+        /* create task on first open */
+        if (!flexio->rx_task)
+        {
+            /* we need this task to be higher priority than HandleSuspendTask in main,
+             * in order to exit out of LPUART_RTOS_Receive safely as suspend deinits it */
+            BUILD_BUG_ON(TTY_RX_TASK_PRIORITY <= SUSPEND_TASK_PRIORITY);
+            xTaskCreate(flexio_tty_rx_task, "flexio rx task", 256U, (void *)settings, TTY_RX_TASK_PRIORITY,
+                        &flexio->rx_task);
+        }
+        else
+        {
+            vTaskResume(flexio->rx_task);
+        }
+    }
+
     return 0;
 }
 
@@ -254,8 +286,6 @@ static int flexio_init_device(struct tty_settings *settings)
         PRINTF("port %d flexio handle init fail: %d\r\n", settings->port_idx, rc);
         return rc;
     }
-
-    flexio->device_init = true;
 
     return 0;
 }
@@ -328,34 +358,23 @@ static int flexio_init(struct tty_settings *settings, struct srtm_tty_init_paylo
     if (rc)
         return rc;
 
-    /* we need this task to be higher priority than HandleSuspendTask in main,
-     * in order to exit out of LPUART_RTOS_Receive safely as suspend deinits it */
-    BUILD_BUG_ON(TTY_RX_TASK_PRIORITY <= SUSPEND_TASK_PRIORITY);
-    xTaskCreate(flexio_tty_rx_task, "flexio rx task", 256U, (void *)settings, TTY_RX_TASK_PRIORITY, &flexio->rx_task);
-
     return 0;
-}
-
-void flexio_suspendTask(struct tty_settings *settings)
-{
-    struct flexio_tty_settings *flexio = get_flexio(settings);
-
-    flexio->in_suspend = true;
 }
 
 void flexio_resumeTask(struct tty_settings *settings)
 {
     struct flexio_tty_settings *flexio = get_flexio(settings);
 
-    flexio->in_suspend = false;
-    vTaskResume(flexio->rx_task);
+    if (settings->state & TTY_ACTIVE)
+    {
+        vTaskResume(flexio->rx_task);
+    }
 }
 
 void flexio_suspend(struct tty_settings *settings)
 {
     struct flexio_tty_settings *flexio = get_flexio(settings);
 
-    flexio->device_init = false;
     FLEXIO_UART_Deinit(&flexio->uart_dev);
 
     if (flexio->suspend_wakeup_gpio == -1)
@@ -384,8 +403,8 @@ const struct tty_hooks tty_flexio_hooks = {
     .tx            = flexio_tx,
     .setcflag      = flexio_setcflag,
     .setwake       = flexio_setwake,
+    .activate      = flexio_activate,
     .init          = flexio_init,
-    .suspendTask   = flexio_suspendTask,
     .resumeTask    = flexio_resumeTask,
     .suspend       = flexio_suspend,
     .resume        = flexio_resume,

srtm/services/srtm_tty_service.c
@@ -45,6 +45,7 @@ typedef struct _srtm_tty_service
     srtm_tty_service_set_cflag_t setCflag;
     srtm_tty_service_set_wake_t setWake;
     srtm_tty_service_init_t init;
+    srtm_tty_service_activate_t activate;
     srtm_channel_t channel;
 } * srtm_tty_service_t;
 
@@ -87,6 +88,7 @@ enum tty_rpmsg_header_cmd
     TTY_RPMSG_COMMAND_NOTIFY,
     TTY_RPMSG_COMMAND_SET_WAKE,
     TTY_RPMSG_COMMAND_INIT,
+    TTY_RPMSG_COMMAND_ACTIVATE,
 };
 
 /*******************************************************************************
@@ -209,6 +211,21 @@ static srtm_status_t SRTM_TtyService_Request(srtm_service_t service, srtm_reques
                 retCode = kStatus_InvalidArgument;
             }
             break;
+        case TTY_RPMSG_COMMAND_ACTIVATE:
+            if (payload->len == sizeof(bool) && handle->setWake != NULL)
+            {
+                SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_DEBUG, "tty %d activate %d\r\n", port_idx, payload->buf[0]);
+                status  = handle->activate(port_idx, payload->buf[0]);
+                retCode = MIN(status, 255);
+            }
+            else
+            {
+                SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_WARN,
+                                   "%s: Command activate not allowed?!? (or bad len %d, expected %zu)\r\n", __func__,
+                                   payload->len, sizeof(bool));
+                retCode = kStatus_InvalidArgument;
+            }
+            break;
         default:
             SRTM_DEBUG_MESSAGE(SRTM_DEBUG_VERBOSE_WARN, "%s: command %d unsupported!\r\n", __func__, command);
             retCode = kStatus_InvalidArgument;
@@ -242,7 +259,8 @@ static srtm_status_t SRTM_TtyService_Notify(srtm_service_t service, srtm_notific
 }
 
 srtm_service_t SRTM_TtyService_Create(srtm_tty_service_tx_t tx, srtm_tty_service_set_cflag_t setCflag,
-                                      srtm_tty_service_set_wake_t setWake, srtm_tty_service_init_t init)
+                                      srtm_tty_service_set_wake_t setWake, srtm_tty_service_init_t init,
+                                      srtm_tty_service_activate_t activate)
 {
     srtm_tty_service_t handle;
 
@@ -255,6 +273,7 @@ srtm_service_t SRTM_TtyService_Create(srtm_tty_service_tx_t tx, srtm_tty_service
     handle->setCflag = setCflag;
     handle->setWake  = setWake;
     handle->init     = init;
+    handle->activate = activate;
 
     SRTM_List_Init(&handle->service.node);
     handle->service.dispatcher = NULL;

srtm/services/srtm_tty_service.h
@@ -73,6 +73,7 @@ typedef int (*srtm_tty_service_tx_t)(uint8_t port_idx, uint8_t *buf, uint16_t le
 typedef int (*srtm_tty_service_set_cflag_t)(uint8_t port_idx, tcflag_t cflag);
 typedef int (*srtm_tty_service_set_wake_t)(uint8_t port_idx, bool enable);
 typedef int (*srtm_tty_service_init_t)(uint8_t port_idx, struct srtm_tty_init_payload *init);
+typedef int (*srtm_tty_service_activate_t)(uint8_t port_idx, bool active);
 
 /*******************************************************************************
  * API
@@ -87,7 +88,8 @@ extern "C" {
  * @return SRTM service handle on success and NULL on failure.
  */
 srtm_service_t SRTM_TtyService_Create(srtm_tty_service_tx_t tx, srtm_tty_service_set_cflag_t setCflag,
-                                      srtm_tty_service_set_wake_t setWake, srtm_tty_service_init_t init);
+                                      srtm_tty_service_set_wake_t setWake, srtm_tty_service_init_t init,
+                                      srtm_tty_service_activate_t activate);
 
 /*!
  * @brief Destroy TTY service.

commit c7de65646fe481113f0f5fcf5fc90bb027feb801

app_tty: do not fail setcflags on resume
lpuart_init_device calls setcflag to restore the baud rate and other
settings after resume, but the new check for suspend state made that
fail inconditionally.

Split setcflags into a function that actually does the job and the linux
call that can check for suspend state

Fixes: 9e7142750507 ("app_tty: add activate call")
Change-Id: Ib84c85609e91b7d4ceb6540e32c95cd3155b1a96

app_tty_lpuart.c
@@ -102,7 +102,7 @@ static int lpuart_tx(struct tty_settings *settings, uint8_t *buf, uint16_t len)
     return rc;
 }
 
-static int lpuart_setcflag(struct tty_settings *settings, tcflag_t cflag)
+static int setcflag(struct tty_settings *settings, tcflag_t cflag)
 {
     struct lpuart_tty_settings *lpuart = get_lpuart(settings);
     speed_t baudrate                   = tty_baudrate(cflag);
@@ -110,12 +110,6 @@ static int lpuart_setcflag(struct tty_settings *settings, tcflag_t cflag)
     bool cmsparity                     = tty_cmsparity(cflag);
     lpuart_data_bits_t databits        = tty_databits(cflag);
     lpuart_stop_bit_count_t stopbits   = tty_stopbits(cflag);
-
-    if (settings->state & TTY_SUSPENDED)
-    {
-        PRINTF("tty %d setcflags while not ready (state %x)\r\n", settings->port_idx, settings->state);
-        return kStatus_Fail;
-    }
 
     /*
      * only support CS8 and CS7, and for CS7 must enable parity.
@@ -151,6 +145,17 @@ static int lpuart_setcflag(struct tty_settings *settings, tcflag_t cflag)
     lpuart->cflag = cflag;
 
     return 0;
+}
+
+static int lpuart_setcflag(struct tty_settings *settings, tcflag_t cflag)
+{
+    if (settings->state & TTY_SUSPENDED)
+    {
+        PRINTF("tty %d setcflags while not ready (state %x)\r\n", settings->port_idx, settings->state);
+        return kStatus_Fail;
+    }
+
+    return setcflag(settings, cflag);
 }
 
 static int lpuart_setwake(struct tty_settings *settings, bool enable)
@@ -221,7 +226,7 @@ static int lpuart_init_device(struct tty_settings *settings)
     LPUART_RTOS_SetRxTimeout(&lpuart->lpuart_rtos_handle, 1, 0); /* short timeout to give data back asap */
     LPUART_RTOS_SetTxTimeout(&lpuart->lpuart_rtos_handle, 0, 0); /* XXX no timeout, depends on baud rate */
 
-    return lpuart_setcflag(settings, lpuart->cflag);
+    return setcflag(settings, lpuart->cflag);
 }
 
 static int lpuart_init(struct tty_settings *settings, struct srtm_tty_init_payload *generic_init)