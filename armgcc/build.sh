atmark-techno/m33-firmware-at

commit a0311572506fe62ae09d644c180e1200afd8db7c

armgcc/build: allow running without env vars
This will allow running  without setting the variables more easily.
If the variables are set (e.g. imx-boot Makefile) there is no visible
change.

Change-Id: Ic99f2502cd06becafda20caa6d2c6d54c400c542

armgcc/build.sh
@@ -9,8 +9,12 @@ workdir="$(dirname "$(readlink -f "$0")")"
 
 cd "$workdir" || exit 1
 
-if [ -z "$SdkRootDirPath" ]; then
-	echo "\$SdkRootDirPath must be set!" >&2
+# default values
+ARMGCC_DIR=${ARMGCC_DIR:-/usr}
+armgcc_cmake="${SdkRootDirPath:-$PWD/../../mcux-sdk}/core/tools/cmake_toolchain_files/armgcc.cmake"
+
+if ! armgcc_cmake=$(realpath -e "$armgcc_cmake"); then
+	echo "Please place m33 firmware next to mcux-sdk directory, or set SdkRootDirPath variable appropriately" >&2
 	exit 1
 fi
 
@@ -43,7 +47,7 @@ esac
 # clear cache unless already configure in same mode with same toolchain
 if ! [ -e Makefile ] \
     || ! grep -q "CMAKE_BUILD_TYPE:STRING=$target" CMakeCache.txt \
-    || ! grep -q "CMAKE_TOOLCHAIN_FILE:FILEPATH=$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" CMakeCache.txt; then
+    || ! grep -q "CMAKE_TOOLCHAIN_FILE:FILEPATH=$armgcc_cmake" CMakeCache.txt; then
 	rm -rf CMakeFiles Makefile cmake_install.cmake CMakeCache.txt "$target/m33-firmware-at.bin"
 	cmake -DCMAKE_TOOLCHAIN_FILE="$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" \
 		-G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$target"

commit 22929fccc8d9d3d8e7cb026555589ca40b0e46ec

armgcc/build: fix to be consistent with armgcc.cmake path
This fix build.sh to avoid rebuilding all files all the time if the
current path is not canonical.
(There is no change in the final binary, just cmake not being rerun ->
files updates being detected properly. We had the realpath in a variable
so use it consistently.)

Fixes: a0311572506f ("armgcc/build: allow running without env vars")
Change-Id: I48e0a0a6785e0875535997caf09c4cb1d0cd7acb

armgcc/build.sh
@@ -49,8 +49,7 @@ if ! [ -e Makefile ] \
     || ! grep -q "CMAKE_BUILD_TYPE:STRING=$target" CMakeCache.txt \
     || ! grep -q "CMAKE_TOOLCHAIN_FILE:FILEPATH=$armgcc_cmake" CMakeCache.txt; then
 	rm -rf CMakeFiles Makefile cmake_install.cmake CMakeCache.txt "$target/m33-firmware-at.bin"
-	cmake -DCMAKE_TOOLCHAIN_FILE="$SdkRootDirPath/core/tools/cmake_toolchain_files/armgcc.cmake" \
-		-G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$target"
+	cmake -DCMAKE_TOOLCHAIN_FILE="$armgcc_cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$target"
 fi
 
 make -j 2>&1 | tee build_log.txt