atmark-techno/m33-firmware-at

commit 07df368e3d365b05146d25e436118c4aad53254a

custom: add more hooks for m33 lifecycle
Add hooks a bit of everywhere around the code for easy prototyping.

This should allow creating simple apps without leaving the custom
folder.

Change-Id: I2360edfa7e657736ca7e7f126634a4166dea3551

custom/custom.c
@@ -6,6 +6,43 @@
  */
 
 #include "app_gpio.h"
+#include "custom.h"
+
+/* This file provides a base for custom application development (that does nothing at all).
+ *
+ * These functions are run at various point during lifecycle:
+ * - custom_early_init(): m33 startup
+ * - custom_init(): m33 startup, after srtm/drivers init
+ * - custom_linux_boot(): linux startup
+ * - custom_early_suspend()/custom_suspend()/custom_resume()/custom_late_resume():
+ *   a35 (linux) suspend/resume, specifically:
+ *   * linux announces suspend (spurrious wakeup sources disabling)
+ *   * A35 core poweroff
+ *   * M33 signals A35 to wakeup (before srtm drivers init)
+ *   * M33 signals A35 to wakeup (after srtm drivers init)
+ * - custom_m33_suspend()/custom_m33_resume(): m33 low power mode enter/exit
+ *
+ * From there one can create FreeRTOS tasks/timers and implement features as appropriate.
+ */
+
+/* global variables */
+// portTickType lastIrqTick;
+
+void custom_early_init(void) {}
+void custom_init(void)
+{
+    /* register GPIOs etc */
+    // APP_GPIO_PinctrlSet(1, 12, IOMUXC_PTB12_PTB12, IOMUXC_PCR_PE_MASK | IOMUXC_PCR_PS_MASK);
+    // APP_GPIO_SetupGPIO_Input(1, 12);
+    // APP_GPIO_SetupIRQ(1, 12, kRGPIO_InterruptRisingEdge, APP_GPIO_IRQCallback_Custom);
+}
+void custom_linux_boot(void) {}
+void custom_early_suspend(void) {}
+void custom_suspend(void) {}
+void custom_resume(void) {}
+void custom_late_resume(void) {}
+void custom_m33_suspend(lpm_rtd_power_mode_e targetMode) {}
+void custom_m33_resume(lpm_rtd_power_mode_e targetMode) {}
 
 /* This function is called when and IRQ fires after having been registered with
  *   APP_GPIO_SetupIRQ(gpioIdx, pinIdx, edge, APP_GPIO_IRQCallback_Custom);

app_srtm.c
@@ -9,6 +9,7 @@
 #include "task.h"
 #include "timers.h"
 #include "main.h"
+#include "custom.h"
 
 #include "srtm_dispatcher.h"
 #include "srtm_peercore.h"
@@ -909,6 +910,7 @@ void APP_SRTM_EarlySuspend(void)
 #ifdef DEBUG_SUSPEND
     PRINTF("%s\r\n", __func__);
 #endif
+    custom_early_suspend();
     APP_TTY_Suspend();
     APP_CAN_Suspend();
 }
@@ -918,6 +920,7 @@ void APP_SRTM_Suspend(void)
 #ifdef DEBUG_SUSPEND
     PRINTF("%s\r\n", __func__);
 #endif
+    custom_suspend();
     APP_WDOG_Suspend();
 }
 
@@ -928,6 +931,7 @@ void APP_SRTM_LateResume(void)
 #endif
     APP_TTY_Resume();
     APP_CAN_Resume();
+    custom_late_resume();
 }
 
 void APP_SRTM_Resume(void)
@@ -935,6 +939,7 @@ void APP_SRTM_Resume(void)
 #ifdef DEBUG_SUSPEND
     PRINTF("%s\r\n", __func__);
 #endif
+    custom_resume();
     APP_WDOG_Resume();
     APP_I2C_Resume();
     /*

armgcc/CMakeLists.txt
@@ -140,6 +140,7 @@ add_executable(${MCUX_SDK_PROJECT_NAME}
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${ProjDirPath}/../custom
     ${ProjDirPath}/..
     ${ProjDirPath}/../3rdparty/printf
     ${ProjDirPath}/../boards/a900

custom/custom.h
@@ -5,3 +5,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #pragma once
+
+#include "lpm.h"
+
+void custom_early_init(void);
+void custom_init(void);
+void custom_linux_boot(void);
+void custom_early_suspend(void);
+void custom_suspend(void);
+void custom_resume(void);
+void custom_late_resume(void);
+void custom_m33_suspend(lpm_rtd_power_mode_e targetMode);
+void custom_m33_resume(lpm_rtd_power_mode_e targetMode);

main.c
@@ -28,6 +28,7 @@
 #include "cli.h"
 #include "lpm.h"
 #include "main.h"
+#include "custom.h"
 #include "fsl_rtd_cmc.h"
 #include "fsl_sentinel.h"
 #include "fsl_wuu.h"
@@ -466,6 +467,7 @@ void APP_PowerPreSwitchHook(lpm_rtd_power_mode_e targetMode)
 #ifdef DEBUG_SUSPEND
     PRINTF("%s\r\n", __func__);
 #endif
+    custom_m33_suspend(targetMode);
     if ((LPM_PowerModeActive != targetMode))
     {
         PRINTF("Preparing for %s\r\n", s_modeNames[targetMode]);
@@ -507,6 +509,7 @@ void APP_PowerPostSwitchHook(lpm_rtd_power_mode_e targetMode, bool result)
         BOARD_SetTrdcGlobalConfig();
         BOARD_SetTrdcAfterApdReset();
     }
+    custom_m33_resume(targetMode);
 }
 static inline const char *APP_GetAllowCombiName(allow_combi_e allow)
 {
@@ -1023,6 +1026,8 @@ int main(void)
     RESET_PeripheralReset(kRESET_Lpi2c1);
     // RESET_PeripheralReset(kRESET_Lpi2c2); // Secure Element
 
+    custom_early_init();
+
     APP_SRTM_Init();
 
     /* register callback for restart the app task when A35 reset */
@@ -1069,6 +1074,7 @@ int main(void)
 #endif /* MCMGR_USED */
 
     APP_CreateTask();
+    custom_init();
 
     /* Start FreeRTOS scheduler. */
     vTaskStartScheduler();

commit 30914f75b139e2b3196596734de7ac70d734834b

doc: improve custom documentation
- add custom/custom.c to readme
- improve comments in custom.c
- improve comments in app_gpio.h that doubles as internal interface to
  gpio

Fixes: 07df368e3d36 ("custom: add more hooks for m33 lifecycle")
Change-Id: I11b3d4c646c857ff92a38a86d43697ee35be7b07

app_gpio.h
@@ -10,13 +10,29 @@
 #include "fsl_wuu.h"
 #include "fsl_iomuxc.h"
 
-/* driver m33 API */
+/* Internal variables, ignore */
+extern RGPIO_Type *const gpios[];
+extern const uint8_t wuuPins[];
+
+/************************************
+ * API for usage within m33 firmware
+ ************************************/
+/* GPIOs are shared with linux and it is important to register
+ * the pinctrl through this function to ensure linux cannot use it.
+ * Example usage: APP_GPIO_PinctrlSet(IOMUXC_PTB12_PTB12, IOMUXC_PCR_PE_MASK | IOMUXC_PCR_PS_MASK);
+ */
 int APP_GPIO_PinctrlSet(uint32_t pinctrl0, uint32_t pinctrl1, uint32_t pinctrl2, uint32_t pinctrl3, uint32_t pinctrl4,
                         uint32_t pinctrl5);
 
-/* Not used directly anymore, use APP_GPIO_write/read below */
-extern RGPIO_Type *const gpios[];
+/* Direction must be set before reading or writing */
+void APP_GPIO_SetupGPIO_Input(uint8_t gpioIdx, uint8_t pinIdx);
+void APP_GPIO_SetupGPIO_Output(uint8_t gpioIdx, uint8_t pinIdx, uint8_t value);
 
+/* read and write to gpio after having set direction.
+ * Example:
+ *   APP_GPIO_Write(gpioIdx, pinIdx, 1);
+ *   value = APP_GPIO_Read(gpioIdx, pinIdx);
+ */
 static inline void APP_GPIO_Write(uint8_t gpioIdx, uint8_t pinIdx, uint8_t output)
 {
     RGPIO_PinWrite(gpios[gpioIdx], pinIdx, output);
@@ -26,20 +42,41 @@ static inline uint32_t APP_GPIO_Read(uint8_t gpioIdx, uint8_t pinIdx)
     return RGPIO_PinRead(gpios[gpioIdx], pinIdx);
 }
 
-void APP_GPIO_SetupGPIO_Input(uint8_t gpioIdx, uint8_t pinIdx);
-void APP_GPIO_SetupGPIO_Output(uint8_t gpioIdx, uint8_t pinIdx, uint8_t value);
-
+/* Register IRQ, for custom app to be used with something such as:
+ *   APP_GPIO_SetupIRQ(gpioIdx, pinIdx, edge, APP_GPIO_IRQCallback_Custom);
+ * where edge is one of:
+ *   kRGPIO_InterruptOrDMADisabled        // Interrupt/DMA request is disabled.
+ *   kRGPIO_DMARisingEdge                 // DMA request on rising edge.
+ *   kRGPIO_DMAFallingEdge                // DMA request on falling edge.
+ *   kRGPIO_DMAEitherEdge                 // DMA request on either edge.
+ *   kRGPIO_FlagRisingEdge                // Flag sets on rising edge.
+ *   kRGPIO_FlagFallingEdge               // Flag sets on falling edge.
+ *   kRGPIO_FlagEitherEdge                // Flag sets on either edge.
+ *   kRGPIO_InterruptLogicZero            // Interrupt when logic zero.
+ *   kRGPIO_InterruptRisingEdge           // Interrupt on rising edge.
+ *   kRGPIO_InterruptFallingEdge          // Interrupt on falling edge.
+ *   kRGPIO_InterruptEitherEdge           // Interrupt on either edge.
+ *   kRGPIO_InterruptLogicOne             // Interrupt when logic one.
+ *   kRGPIO_ActiveHighTriggerOutputEnable // Enable active high-trigger output.
+ *   kRGPIO_ActiveLowTriggerOutputEnable  // Enable active low-trigger output.
+ */
 enum APP_GPIO_IRQCallback
 {
     APP_GPIO_IRQCallback_Linux,  /* reserved for srtm driver */
     APP_GPIO_IRQCallback_Custom, /* calls custom_GPIO_IRQHandler(gpioIdx, pinIdx) on irq */
 };
+void custom_GPIO_IRQHandler(uint8_t gpioIdx, uint8_t pinIdx); // call into custom/custom.c
 void APP_GPIO_SetupIRQ(uint8_t gpioIdx, uint8_t pinIdx, rgpio_interrupt_config_t edge, enum APP_GPIO_IRQCallback cb);
 
+/* Register pin to Wake Up Unit (WUU), to wake up from sleep modes:
+ *   APP_GPIO_SetupWUU(gpioIdx, pinIdx, edge);
+ * where edge is one of:
+ *   kWUU_ExternalPinDisable     // disabled as wake up input.
+ *   kWUU_ExternalPinRisingEdge  // enabled with the rising edge detection.
+ *   kWUU_ExternalPinFallingEdge // enabled with the falling edge detection.
+ *   kWUU_ExternalPinAnyEdge     // enabled with any change detection.
+ */
 void APP_GPIO_SetupWUU(uint8_t gpioIdx, uint8_t pinIdx, wuu_external_pin_edge_detection_t wuuEdge);
-
-/* defined in custom/gpio.c */
-void custom_GPIO_IRQHandler(uint8_t gpioIdx, uint8_t pinIdx);
 
 /* various constants and index manipulation helpers... */
 #define APP_IO_PINS_PER_CHIP 25U
@@ -75,7 +112,6 @@ static inline uint16_t APP_IO_GetId(uint8_t inputIdx)
         return 0xffff;
     return ((inputIdx / APP_IO_PINS_PER_CHIP) << 8U) | (inputIdx % APP_IO_PINS_PER_CHIP);
 }
-extern const uint8_t wuuPins[];
 static inline uint8_t APP_IO_GetWUUPin(uint8_t gpio_idx, uint8_t pin_idx)
 {
     /* only PTA/PTB */

custom/custom.c
@@ -11,9 +11,17 @@
 #include "app_gpio.h"
 #include "custom.h"
 
-/* This file provides a base for custom application development (that does nothing at all).
- *
- * These functions are run at various point during lifecycle:
+/* This file provides a base for custom application development (that does nothing at all). */
+
+/*******************
+ * Global variables
+ *******************/
+// portTickType lastIrqTick;
+
+/*******************
+ * Hooks
+ *******************/
+/* These functions are run at various point during lifecycle:
  * - custom_early_init(): m33 startup
  * - custom_init(): m33 startup, after srtm/drivers init
  * - custom_linux_boot(): linux startup
@@ -25,12 +33,7 @@
  *   * M33 signals A35 to wakeup (after srtm drivers init)
  * - custom_m33_suspend()/custom_m33_resume(): m33 low power mode enter/exit
  *
- * From there one can create FreeRTOS tasks/timers and implement features as appropriate.
- */
-
-/* global variables */
-// portTickType lastIrqTick;
-
+ * From there one can create FreeRTOS tasks/timers and implement features as appropriate. */
 void custom_early_init(void) {}
 void custom_init(void)
 {
@@ -47,24 +50,12 @@ void custom_late_resume(void) {}
 void custom_m33_suspend(lpm_rtd_power_mode_e targetMode) {}
 void custom_m33_resume(lpm_rtd_power_mode_e targetMode) {}
 
-/* This function is called when and IRQ fires after having been registered with
- *   APP_GPIO_SetupIRQ(gpioIdx, pinIdx, edge, APP_GPIO_IRQCallback_Custom);
- * where edge is one of:
- *   kRGPIO_InterruptOrDMADisabled        // Interrupt/DMA request is disabled.
- *   kRGPIO_DMARisingEdge                 // DMA request on rising edge.
- *   kRGPIO_DMAFallingEdge                // DMA request on falling edge.
- *   kRGPIO_DMAEitherEdge                 // DMA request on either edge.
- *   kRGPIO_FlagRisingEdge                // Flag sets on rising edge.
- *   kRGPIO_FlagFallingEdge               // Flag sets on falling edge.
- *   kRGPIO_FlagEitherEdge                // Flag sets on either edge.
- *   kRGPIO_InterruptLogicZero            // Interrupt when logic zero.
- *   kRGPIO_InterruptRisingEdge           // Interrupt on rising edge.
- *   kRGPIO_InterruptFallingEdge          // Interrupt on falling edge.
- *   kRGPIO_InterruptEitherEdge           // Interrupt on either edge.
- *   kRGPIO_InterruptLogicOne             // Interrupt when logic one.
- *   kRGPIO_ActiveHighTriggerOutputEnable // Enable active high-trigger output.
- *   kRGPIO_ActiveLowTriggerOutputEnable  // Enable active low-trigger output.
- */
+/*******************
+ * GPIOs
+ *******************/
+/* Note: GPIOs are shared with linux and must be used with the API defined in app_gpio.h
+ * to avoid conflicts */
+/* This function is called when and IRQ has been registered with APP_GPIO_IRQCallback_Custom */
 void custom_GPIO_IRQHandler(uint8_t gpioIdx, uint8_t pinIdx)
 {
     /* note: asserts are only compiled in debug build */