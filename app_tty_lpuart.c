atmark-techno/m33-firmware-at

commit d9c74e036dff236789bcf700d8af414c35b2ce74

app_tty_lpuart: Stop receive before suspending
If you suspend during reception, it will stop at Deinit. This can be
reproduced as follows:

[a9e]# cat /dev/ttyrpmsg1 &
[a9e]# aiot-sleep

Deinit will not finish while receive is in progress, it must wait for
suspend first.

The timing of deinit has changed in 4c24439abec3 ("suspend: split
APP_SRTM_Suspend into EarlySuspend/Suspend"), making it more likely to
cause problems.

Change-Id: Ie7ee31d9b664d6fe92037bd780e6fa48450a950b

app_tty_lpuart.c
@@ -323,6 +323,13 @@ void lpuart_suspend(struct tty_settings *settings)
 {
     struct lpuart_tty_settings *lpuart = get_lpuart(settings);
 
+    /* Deinit will not finish while receive is in progress, it must
+     * wait for suspend first. */
+    vTaskPrioritySet(lpuart->rx_task, configMAX_PRIORITIES - 1);
+    while (eTaskGetState(lpuart->rx_task) != eSuspended)
+        ;
+    vTaskPrioritySet(lpuart->rx_task, TTY_RX_TASK_PRIORITY);
+
     LPUART_RTOS_Deinit(&lpuart->lpuart_rtos_handle);
 
     if (lpuart->suspend_wakeup_gpio == -1)

commit bd7e0eba40f46dcf5af7d13f497dbcefca91b89f

app_tty_lpuart: Fixed inability to suspend before activate
Since rx_task does not exist, it goes into an infinite loop.

Fixes: d9c74e036dff ("app_tty_lpuart: Stop receive before suspending")
Change-Id: Id7050d087f6154d192463bbd8e39fdc86a7c741b

app_tty_lpuart.c
@@ -324,10 +324,13 @@ void lpuart_suspend(struct tty_settings *settings)
 
     /* Deinit will not finish while receive is in progress, it must
      * wait for suspend first. */
-    vTaskPrioritySet(lpuart->rx_task, configMAX_PRIORITIES - 1);
-    while (eTaskGetState(lpuart->rx_task) != eSuspended)
-        ;
-    vTaskPrioritySet(lpuart->rx_task, TTY_RX_TASK_PRIORITY);
+    if (lpuart->rx_task)
+    {
+        vTaskPrioritySet(lpuart->rx_task, configMAX_PRIORITIES - 1);
+        while (eTaskGetState(lpuart->rx_task) != eSuspended)
+            ;
+        vTaskPrioritySet(lpuart->rx_task, TTY_RX_TASK_PRIORITY);
+    }
 
     LPUART_RTOS_Deinit(&lpuart->lpuart_rtos_handle);
 